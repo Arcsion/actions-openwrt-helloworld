b4bebb2315772cb5ed7cb51eb377ec104c51d6b1
ArcsionLin <40288853+Arcsion@users.noreply.github.com>
2024-12-23 23:11:35 +0800

    local

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,4 +13,4 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git openwrt-packages https://github.com/sundaqiang/openwrt-packages' >>feeds.conf.default
